90352c562ad810e0ebdc07c497eb821bc25e4a45
iampranaywaghmare <122095004+iampranaywaghmare@users.noreply.github.com>
2023-01-06 17:50:44 +0530

    Update First.java

--- First.java
+++ First.java
@@ -1,7 +1,24 @@
-class First{
+class MethodOverloading{
+// two number addition
+void addition(int a, int b){
+int result = a + b;
+System.out.println("Result ="+ result);
+}
+// three number addition
+void addition(int a, int b, int c){
+int result = a + b + c;
+System.out.println("Result ="+ result);
+}
+//four number addition
+void addition(int a, int b, int c, int d){
+int result = a + b + c + d;
+System.out.println("Result ="+ result);
+}
+}
+class MethodOverriding{
+
+}class First{
 public static void main(String args[]){
-
-System.out.print("Hello Java");
-
+MethodOverloading test = new MethodOverloading(); 
 }
 }
